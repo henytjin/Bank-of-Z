9f0150baffc77b384eb0ff6e92292e47743f247d
Heny Tjin <htjin@ca.ibm.com>
2026-06-01 18:00:46 -0400

    ZDTS-14923 install only what's needed

--- htjin_utils/tools/install-tools.sh
+++ htjin_utils/tools/install-tools.sh
@@ -29,78 +29,87 @@ set -e
 # Directory containing this script (and the installation archives).
 SCRIPT_DIR=$(cd "$(dirname "$0")" 2>/dev/null && pwd)
 TOOLS_BIN_DIR=$SCRIPT_DIR
+TARGET_INSTALL_DIR="/u/ibmuser/sandboxes/bank-of-z/tools"
+
+# --- Variables --------------------------------------------------------------
+# Nexus Manzanita credentials
+CREDENTIALS="$1"
+# Update TAZ
+UPDATE_TAZ="${2:-False}"
 
 # ============================================================
 # 1. Create or refresh the sandbox zFS
 # ============================================================
-if [ -d /usr/local/sandboxes ]; then
-    echo "==> /usr/local/sandboxes already exist."
-else
-    echo "==> Creating or refreshing the sandbox zFS"
-    sh ./mkzfs.sh -q SANDBOX -p 5000 -d /usr/local/sandboxes
+# if [ -d /usr/local/sandboxes ]; then
+#     echo "==> /usr/local/sandboxes already exist."
+# else
+#     echo "==> Creating or refreshing the sandbox zFS"
+#     sh ./mkzfs.sh -q SANDBOX -p 5000 -d /usr/local/sandboxes
 
-fi
+# fi
 
-rm -rf /usr/local/sandboxes/tools
-mkdir -p /usr/local/sandboxes/tools
+# rm -rf /usr/local/sandboxes/tools
+# mkdir -p /usr/local/sandboxes/tools
 
-# Increase the OMVS MEMLIMIT for IBMUSER (required by npm and Java workloads).
+# # Increase the OMVS MEMLIMIT for IBMUSER (required by npm and Java workloads).
 tsocmd "ALU IBMUSER OMVS(MEMLIMIT(32768M))"
 
 # ============================================================
 # 2. Add extra page sets
 # ============================================================
-#echo "==> Adding extra page sets"
-#MAX_PAGES_SETS=7
-#FAILED=0
-#i=1
-#while [ "$i" -le "$((MAX_PAGES_SETS - 1))" ]; do
-#  echo "--- Page set [$i/$((MAX_PAGES_SETS - 1))] ---"
-#  if ./add-pageset.sh --apply --max-pages "$MAX_PAGES_SETS"; then
-#    echo "[$i] OK"
-#  else
-#    RC=$?
-#    FAILED=$((FAILED + 1))
-#    echo "ERROR: add-pageset.sh failed at iteration $i (rc=$RC)"
-#    exit $RC
-#  fi
-#  i=$((i + 1))
-#done
+echo "==> Adding extra page sets"
+MAX_PAGES_SETS=7
+FAILED=0
+i=1
+while [ "$i" -le "$((MAX_PAGES_SETS - 1))" ]; do
+ echo "--- Page set [$i/$((MAX_PAGES_SETS - 1))] ---"
+ if ./add-pageset.sh --apply --max-pages "$MAX_PAGES_SETS"; then
+   echo "[$i] OK"
+ else
+   RC=$?
+   FAILED=$((FAILED + 1))
+   echo "ERROR: add-pageset.sh failed at iteration $i (rc=$RC)"
+   exit $RC
+ fi
+ i=$((i + 1))
+done
 
 # ============================================================
 # 3. Download archives from Nexus
 # ============================================================
-echo "==> Downloading tools"
-AUTH="$1"
-BASE_URL="http://zdevops-demo1.fyre.ibm.com:8888/repository/manzanita/tools"
+# echo "==> Downloading tools"
+# AUTH="$1"
+# BASE_URL="http://zdevops-demo1.fyre.ibm.com:8888/repository/manzanita/tools"
 
-files=(
-  "gradle-9.5.1-bin.zip"
-  "ibm-semeru-certified-jdk_s390x_zos_21.0.10.1.pax.Z"
-  "zconfig-0.4.1.dev1-py3-none-any.whl"
-  "cics-resource-builder-1.0.6.zip"
-  "wazideploy-3.0.7.3-py3.14-none-any.whl"
-  "dbb-3.0.4.1-war-fix.tar"
-)
+# files=(
+#   "gradle-9.5.1-bin.zip"
+#   "ibm-semeru-certified-jdk_s390x_zos_21.0.10.1.pax.Z"
+#   "zconfig-0.4.1.dev1-py3-none-any.whl"
+#   "cics-resource-builder-1.0.6.zip"
+#   "wazideploy-3.0.7.3-py3.14-none-any.whl"
+#   "dbb-3.0.4.1-war-fix.tar"
+# )
 
-for file in "${files[@]}"; do
-  if [ -f "$file" ]; then
-    echo "  $file already exists, skipping."
-  else
-    echo "  Downloading $file..."
-    curl -u "$AUTH" -O "$BASE_URL/$file"
-    chtag -r "$file"
-  fi
-done
+# for file in "${files[@]}"; do
+#   if [ -f "$file" ]; then
+#     echo "  $file already exists, skipping."
+#   else
+#     echo "  Downloading $file..."
+#     curl -u "$AUTH" -O "$BASE_URL/$file"
+#     chtag -r "$file"
+#   fi
+# done
+
 
 # ============================================================
 # 4. Install ZConfig (dedicated Python virtual environment)
 # ============================================================
 echo "==> Installing ZConfig"
-cd /usr/local/sandboxes/tools/
+cd $TARGET_INSTALL_DIR
+export ZOAU_HOME=$TARGET_INSTALL_DIR/zoautils
 python3 -m venv zconfig
 . zconfig/bin/activate
-pip3 install "$ZOAU_HOME/zoautil_py-1.4.1.0-cp314-cp314-zos.whl"
+pip3 install "$ZOAU_HOME/zoautil_py-1.4.1.1-cp314-cp314-zos.whl"
 pip3 install "$TOOLS_BIN_DIR/zconfig-0.4.1.dev1-py3-none-any.whl"
 pip3 list
 deactivate
@@ -108,19 +117,19 @@ deactivate
 # ============================================================
 # 5. Install Wazi Deploy (GDP Python environment)
 # ============================================================
-echo "==> Installing Wazi Deploy"
-cd /usr/local/sandboxes/tools/
-. /global/opt/pyenv/gdp/bin/activate
-pip3 install "$TOOLS_BIN_DIR/wazideploy-3.0.7.3-py3.14-none-any.whl"
-pip3 install "$ZOAU_HOME/zoautil_py-1.4.1.0-cp314-cp314-zos.whl"
-pip3 list
-deactivate
+# echo "==> Installing Wazi Deploy"
+# cd TARGET_INSTALL_DIR
+# . /global/opt/pyenv/gdp/bin/activate
+# pip3 install "$TOOLS_BIN_DIR/wazideploy-3.0.7.3-py3.14-none-any.whl"
+# pip3 install "$ZOAU_HOME/zoautil_py-1.4.1.0-cp314-cp314-zos.whl"
+# pip3 list
+# deactivate
 
 # ============================================================
 # 6. Install the CICS Resource Builder
 # ============================================================
 echo "==> Installing CICS Resource Builder"
-cd /usr/local/sandboxes/tools/
+cd $TARGET_INSTALL_DIR
 mkdir -p zrb && cd zrb
 jar xf "$TOOLS_BIN_DIR/cics-resource-builder-1.0.6.zip"
 chmod +x cics-resource-builder-1.0.6/bin/zrb
@@ -131,29 +140,29 @@ cics-resource-builder-1.0.6/bin/zrb
 # 7. Install Java 21.0.10.1
 # Note: Java 21 on ZDVT has encoding issues with Gradle.
 # ============================================================
-echo "==> Installing Java 21.0.10.1"
-cd /usr/local/sandboxes/tools/
-pax -pp -rzf "$TOOLS_BIN_DIR/ibm-semeru-certified-jdk_s390x_zos_21.0.10.1.pax.Z"
-extattr +a $(find J21.0_64 -name libj9ifa29.so)
+# echo "==> Installing Java 21.0.10.1"
+# cd TARGET_INSTALL_DIR
+# pax -pp -rzf "$TOOLS_BIN_DIR/ibm-semeru-certified-jdk_s390x_zos_21.0.10.1.pax.Z"
+# extattr +a $(find J21.0_64 -name libj9ifa29.so)
 
 # ============================================================
 # 8. Install Gradle
 # ============================================================
 echo "==> Installing Gradle"
-cd /usr/local/sandboxes/tools/
+cd $TARGET_INSTALL_DIR
 jar xf "$TOOLS_BIN_DIR/gradle-9.5.1-bin.zip"
 chmod +x gradle-9.5.1/bin/gradle
-export JAVA_HOME=/usr/local/sandboxes/tools/J21.0_64
+export JAVA_HOME=/usr/lpp/java/java21/current_64
 export GRADLE_OPTS="-Dfile.encoding=UTF-8"
 export GRADLE_USER_HOME=$PWD
-/usr/local/sandboxes/tools/gradle-9.5.1/bin/gradle build help
+$TARGET_INSTALL_DIR/gradle-9.5.1/bin/gradle build help
 
 
 # ============================================================
 # 9. Install DBB Fix for WAR file packing
 # ============================================================
 echo "==> Install DBB Fix for WAR file packing"
-cd /usr/local/sandboxes/tools/
+cd $TARGET_INSTALL_DIR
 tar -xf "$TOOLS_BIN_DIR/dbb-3.0.4.1-war-fix.tar"
 
 # ============================================================
